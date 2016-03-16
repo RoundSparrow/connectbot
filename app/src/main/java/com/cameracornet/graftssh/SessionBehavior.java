d7f119ede967b264f8022152066326a390c6e218
Stephen A. Gutknecht <stephen@hilltx.com>
2016-03-16 06:52:31 -0400

    SessionBehavior.java

---
+++ app/src/main/java/com/cameracornet/graftssh/SessionBehavior.java
@@ -0,0 +1,45 @@
+package com.cameracornet.graftssh;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import org.connectbot.bean.HostBean;
+
+/**
+ * Created by adminsag on 3/14/16.
+ */
+public class SessionBehavior {
+	public static final String TAG = "CB.SessionBehavior";
+
+	public static int connectWhenInThisNetworkCondition(Context context, HostBean host) {
+		boolean anyPathToInternet = hasInternetConnection(context);
+		if (! anyPathToInternet) {
+			android.util.Log.w(TAG, "network unavailable being told before connect, Host " + host.toXML());
+			return 2;
+		}
+		return 0;
+	}
+
+
+	public static boolean hasInternetConnection(Context context)
+	{
+		ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo wifiNetwork = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+		if (wifiNetwork != null && wifiNetwork.isConnected())
+		{
+			return true;
+		}
+		NetworkInfo mobileNetwork = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+		if (mobileNetwork != null && mobileNetwork.isConnected())
+		{
+			return true;
+		}
+		NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+		if (activeNetwork != null && activeNetwork.isConnected())
+		{
+			return true;
+		}
+		return false;
+	}
+}
